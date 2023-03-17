004bdc27a17c4a945544815f9805ee7c6c0b96d7
Haile-08 <haile4cmd@gmail.com>
2023-03-17 15:41:38 +0300

    Made the interface

--- Todo_list/src/App.jsx
+++ Todo_list/src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./styles/App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="main">
+      <div className="input_todo">
+        <div className="input_area">
+          <input type="text" placeholder="Enter task"></input>
+        </div>
+        <div className="btn">
+          <button>add</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="display_todo">
+        <div className="todos">
+          <p>tot</p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
