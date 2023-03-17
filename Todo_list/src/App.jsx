18a5a79f02247d2d9e38493bfa77ee13ccac1f54
Haile-08 <haile4cmd@gmail.com>
2023-03-17 18:05:10 +0300

    Manage state with hooks

--- Todo_list/src/App.jsx
+++ Todo_list/src/App.jsx
@@ -1,20 +1,38 @@
+import { useState } from "react";
 import "./styles/App.css";
+import data from "./data/data.json";
 
 function App() {
+  const [i, setI] = useState("");
+  const [todo, setTodo] = useState(data);
+
+  function handelClick() {
+    let copy = [...todo, { todol: i }];
+    setTodo(copy);
+  }
   return (
     <div className="main">
       <div className="input_todo">
         <div className="input_area">
-          <input type="text" placeholder="Enter task"></input>
+          <input
+            type="text"
+            placeholder="Enter task"
+            value={i}
+            onChange={(e) => setI(e.target.value)}
+          ></input>
         </div>
         <div className="btn">
-          <button>add</button>
+          <button onClick={handelClick}>add</button>
         </div>
       </div>
       <div className="display_todo">
-        <div className="todos">
-          <p>tot</p>
-        </div>
+        {todo.map((todo) => {
+          return (
+            <div className="todos">
+              <p>{todo.todol}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
