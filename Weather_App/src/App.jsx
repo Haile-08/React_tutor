b12f4c3dcc640baf44523ffe1194d1cc942e18af
Haile-08 <haile4cmd@gmail.com>
2023-03-24 16:35:54 +0300

    Setup the api key

--- Weather_App/src/App.jsx
+++ Weather_App/src/App.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import Days from "./Days";
 import "./style/index.css";
+const API_KEY = "3695967c219031489fe97e3b33166059";
 
 function App() {
+  const [todayData, setTodayData] = useState("");
+
+  function success(pos) {
+    const crd = pos.coords;
+    setTodayData(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=${API_KEY}`
+    );
+  }
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error);
+  console.log(todayData);
+
   return (
     <div className="App">
       <div className="today">
