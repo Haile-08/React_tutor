0ebd49cc3279590bbc3f14d5e8b296086accf76c
Haile-08 <haile4cmd@gmail.com>
2023-03-24 20:00:44 +0300

    Add assets

--- Weather_App/src/App.jsx
+++ Weather_App/src/App.jsx
@@ -1,9 +1,19 @@
 import { useEffect, useState } from "react";
 import Days from "./Days";
+import Image from "./Image";
 import "./style/index.css";
 import axios from "axios";
-
 const API_KEY = "3695967c219031489fe97e3b33166059";
+
+//image imports
+import cloud from "./assets/cloud.png";
+import rain from "./assets/rain.png";
+import sunny from "./assets/sunny.png";
+
+import rain_image from "./assets/Rainingimage.png";
+import strom_image from "./assets/Stormimage.png";
+import sunny_image from "./assets/Sunnyimage.png";
+
 function App() {
   const [todayData, setTodayData] = useState("");
   const [weekData, setWeekData] = useState("");
@@ -50,19 +60,40 @@ function App() {
       });
   };
 
+  const Kelvin_to_Celsius = (temp) => {
+    return temp - 273.15;
+  };
+  let image;
+  let symbole;
+  if (Kelvin_to_Celsius(todayFetch.main.temp) > 23) {
+    symbole = <Image image={sunny} />;
+    image = <Image image={sunny_image} />;
+  } else if (
+    Kelvin_to_Celsius(todayFetch.main.temp) < 23 &&
+    Kelvin_to_Celsius(todayFetch.main.temp) > 13
+  ) {
+    symbole = <Image image={cloud} />;
+    image = <Image image={strom_image} />;
+  } else {
+    symbole = <Image image={rain} />;
+    image = <Image image={rain_image} />;
+  }
+
   console.log(todayFetch);
   console.log(weekFetch);
   return (
     <div className="App">
       <div className="today">
         <div className="output">
-          <div className="symbole"></div>
+          <div className="symbole">{symbole}</div>
           <div className="temp"></div>
           <div className="date"></div>
           <div className="line"></div>
         </div>
         <div className="city">
-          <div className="city_image"></div>
+          <div className="city_image">
+            <img src="" alt="" />
+          </div>
           <p>new york</p>
         </div>
       </div>
