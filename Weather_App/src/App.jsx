06ae299f862a7838b91ff48e20a03b0abc54aa30
Haile-08 <haile4cmd@gmail.com>
2023-03-24 19:27:56 +0300

    Add fetch function

--- Weather_App/src/App.jsx
+++ Weather_App/src/App.jsx
@@ -1,24 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Days from "./Days";
 import "./style/index.css";
-const API_KEY = "3695967c219031489fe97e3b33166059";
+import axios from "axios";
 
+const API_KEY = "3695967c219031489fe97e3b33166059";
 function App() {
   const [todayData, setTodayData] = useState("");
+  const [weekData, setWeekData] = useState("");
+  const [todayFetch, setTodayFetch] = useState(null);
+  const [weekFetch, setWeekFetch] = useState(null);
 
   function success(pos) {
     const crd = pos.coords;
     setTodayData(
       `https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=${API_KEY}`
     );
+    setWeekData(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${crd.latitude}&lon=${crd.longitude}&appid=${API_KEY}`
+    );
   }
   function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
   }
-
   navigator.geolocation.getCurrentPosition(success, error);
-  console.log(todayData);
+  useEffect(() => {
+    getTodayData();
+    getWeekData();
+  }, []);
+  const getTodayData = () => {
+    axios
+      .get(todayData)
+      .then((res) => {
+        const result = res.data;
+        setTodayFetch(result);
+      })
+      .catch((err) => {
+        console.log(`Error: ${err}`);
+      });
+  };
+  const getWeekData = () => {
+    axios
+      .get(weekData)
+      .then((res) => {
+        const result = res.data;
+        setWeekFetch(result);
+      })
+      .catch((err) => {
+        console.log(`Error: ${err}`);
+      });
+  };
 
+  console.log(todayFetch);
+  console.log(weekFetch);
   return (
     <div className="App">
       <div className="today">
@@ -29,6 +62,7 @@ function App() {
           <div className="line"></div>
         </div>
         <div className="city">
+          <div className="city_image"></div>
           <p>new york</p>
         </div>
       </div>
