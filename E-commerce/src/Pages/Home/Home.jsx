a1b4a19e3202e5a93e67f2a055819ebf128ac4ca
Haile-08 <haile4cmd@gmail.com>
2023-04-05 16:12:01 +0300

    Add main intro image

--- E-commerce/src/Pages/Home/Home.jsx
+++ E-commerce/src/Pages/Home/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./Home.css";
+import intro from "../../assets/intro.jpg";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="image">
+        <img src={intro} alt="img" />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
---
+++ E-commerce/src/components/ProductCategory/Productcategory.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Productcategory() {
+  return <div>Productcategory</div>;
+}
+
+export default Productcategory;
