c63e9e9c76ebe85b1d7e81ebd1a824500875fc76
Haile-08 <haile4cmd@gmail.com>
2023-04-06 14:01:25 +0300

    Added a catetory page

--- E-commerce/src/Pages/Home/Home.jsx
+++ E-commerce/src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Home.css";
 import intro from "../../assets/intro.jpg";
+import Productcategory from "../../components/ProductCategory/Productcategory";
 
 function Home() {
   return (
     <div className="home">
       <div className="image">
         <img src={intro} alt="img" />
+        <Productcategory />
       </div>
     </div>
   );
--- E-commerce/src/components/ProductCategory/Productcategory.jsx
+++ E-commerce/src/components/ProductCategory/Productcategory.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import "./Productcategory.css";
+
+// images
+
+import Elec from "../../assets/electronics.jpg";
+import Jew from "../../assets/jewelery.jpg";
+import mens from "../../assets/men.jpg";
+import womens from "../../assets/women.jpg";
 
 function Productcategory() {
-  return <div>Productcategory</div>;
+  return (
+    <div className="category">
+      <div className="title">
+        <p>Category</p>
+      </div>
+      <div className="cards">
+        <div className="card">
+          <img src={Elec} alt="elc" />
+          <p>Electronics</p>
+        </div>
+        <div className="card">
+          <img src={Jew} alt="elc" />
+          <p>jewelery</p>
+        </div>
+        <div className="card">
+          <img src={mens} alt="elc" />
+          <p>Men</p>
+        </div>
+        <div className="card">
+          <img src={womens} alt="elc" />
+          <p>Women</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Productcategory;
