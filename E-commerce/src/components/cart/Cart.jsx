99ab8080806e9bf74020003e1e7005563b629b2c
Haile-08 <haile4cmd@gmail.com>
2023-04-09 11:25:21 +0300

    Add cart remove functionlity

--- E-commerce/src/components/cart/Cart.jsx
+++ E-commerce/src/components/cart/Cart.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Cart.css";
 import { FaTrash } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { removeItem } from "../../features/cart/cartSlice";
 
 function Cart() {
   const products = useSelector((state) => state.cart.products);
+  const dispatch = useDispatch();
+
+  const total = () => {
+    let total = 0;
+    products?.forEach((item) => (total += item.quantity * item.price));
+    return total.toFixed(2);
+  };
   return (
     <div className="cart">
       <p>Products in your cart</p>
@@ -19,7 +27,7 @@ function Cart() {
               <p>{item.desc}</p>
             </div>
           </div>
-          <button>
+          <button onClick={() => dispatch(removeItem(item.id))}>
             <FaTrash />
           </button>
         </div>
@@ -27,7 +35,7 @@ function Cart() {
       <div className="foot">
         <div className="subtotal">
           <p>SUBTOTAL</p>
-          <span>$350</span>
+          <span>${total()}</span>
         </div>
         <div className="checkout">
           <button>PROCEED TO CHECKOUT</button>
